7376338d5933dbfcf1d1f4a04a7a5e86b5f3d256
Milad MP <smmp.milad@gmail.com>
2024-07-06 20:12:42 +0330

    update file

--- MusicPlayerv1/MusicPlayerv1/Form1.Designer.cs
+++ MusicPlayerv1/MusicPlayerv1/Form1.Designer.cs
@@ -34,6 +34,7 @@
             btnNext = new Button();
             btnPlay = new Button();
             btnStop = new Button();
+            btnBrowse = new Button();
             ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
             SuspendLayout();
             // 
@@ -48,17 +49,18 @@
             // listBox1
             // 
             listBox1.FormattingEnabled = true;
-            listBox1.Location = new Point(168, 7);
+            listBox1.Location = new Point(162, 7);
             listBox1.Name = "listBox1";
-            listBox1.Size = new Size(322, 104);
+            listBox1.Size = new Size(349, 84);
             listBox1.TabIndex = 1;
             // 
             // btnPreview
             // 
             btnPreview.Cursor = Cursors.Hand;
-            btnPreview.Location = new Point(168, 124);
+            btnPreview.Font = new Font("Segoe UI", 7.8F);
+            btnPreview.Location = new Point(162, 129);
             btnPreview.Name = "btnPreview";
-            btnPreview.Size = new Size(76, 29);
+            btnPreview.Size = new Size(65, 29);
             btnPreview.TabIndex = 2;
             btnPreview.Text = "Preview";
             btnPreview.UseVisualStyleBackColor = true;
@@ -66,9 +68,10 @@
             // btnNext
             // 
             btnNext.Cursor = Cursors.Hand;
-            btnNext.Location = new Point(250, 124);
+            btnNext.Font = new Font("Segoe UI", 7.8F);
+            btnNext.Location = new Point(233, 129);
             btnNext.Name = "btnNext";
-            btnNext.Size = new Size(76, 29);
+            btnNext.Size = new Size(65, 29);
             btnNext.TabIndex = 3;
             btnNext.Text = "Next";
             btnNext.UseVisualStyleBackColor = true;
@@ -76,9 +79,10 @@
             // btnPlay
             // 
             btnPlay.Cursor = Cursors.Hand;
-            btnPlay.Location = new Point(332, 124);
+            btnPlay.Font = new Font("Segoe UI", 7.8F);
+            btnPlay.Location = new Point(304, 129);
             btnPlay.Name = "btnPlay";
-            btnPlay.Size = new Size(76, 29);
+            btnPlay.Size = new Size(65, 29);
             btnPlay.TabIndex = 4;
             btnPlay.Text = "Play";
             btnPlay.UseVisualStyleBackColor = true;
@@ -86,18 +90,31 @@
             // btnStop
             // 
             btnStop.Cursor = Cursors.Hand;
-            btnStop.Location = new Point(414, 124);
+            btnStop.Font = new Font("Segoe UI", 7.8F);
+            btnStop.Location = new Point(375, 129);
             btnStop.Name = "btnStop";
-            btnStop.Size = new Size(76, 29);
+            btnStop.Size = new Size(65, 29);
             btnStop.TabIndex = 5;
             btnStop.Text = "Stop";
             btnStop.UseVisualStyleBackColor = true;
+            // 
+            // btnBrowse
+            // 
+            btnBrowse.Cursor = Cursors.Hand;
+            btnBrowse.Font = new Font("Segoe UI", 7.8F);
+            btnBrowse.Location = new Point(446, 129);
+            btnBrowse.Name = "btnBrowse";
+            btnBrowse.Size = new Size(65, 29);
+            btnBrowse.TabIndex = 6;
+            btnBrowse.Text = "Browse";
+            btnBrowse.UseVisualStyleBackColor = true;
             // 
             // Form1
             // 
             AutoScaleDimensions = new SizeF(8F, 20F);
             AutoScaleMode = AutoScaleMode.Font;
             ClientSize = new Size(513, 166);
+            Controls.Add(btnBrowse);
             Controls.Add(btnStop);
             Controls.Add(btnPlay);
             Controls.Add(btnNext);
@@ -118,5 +135,6 @@
         private Button btnNext;
         private Button btnPlay;
         private Button btnStop;
+        private Button btnBrowse;
     }
 }
